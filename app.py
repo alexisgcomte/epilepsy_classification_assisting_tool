35eb7bafa7f7a00de847c78984faee7623523ad4
alexisgcomte <alexis.g.comte@gmail.com>
2020-08-02 20:25:03 +0200

    adding function to generate proper markdown sulined report

--- app.py
+++ app.py
@@ -10,46 +10,74 @@ def load_data():
     data = pd.read_csv('0. Data/classification_dataset_hashtag_sample.csv', sep=";")
     return data
 
-
-
 # Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
-data = load_data()
+dataset = load_data()
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Data Loaded in cache successfully!")
 
-def create_markdown(index, data):
-    list_of_colors = []
-    report = data['report'].iloc[index]
-    surline_data = data['surline_data'].iloc[index]
-    surline_data_list = ast.literal_eval(surline_data)
-    start_tag = '<span style="background-color: #FFFF00">'
-    end_tag = '</span>'
-    shift = 0
+def create_markdown_text(report, surline_information):
+
+    try : 
+        # Initialization and parameters
+        shift = 0
+        start_tag = '<span style="background-color: #FFFF00">'
+        end_tag = '</span>'
+
+        # Creating a dataset of the elements to surline
+        df_surline_batch = pd.DataFrame(columns=['word','coordinates'])
+
+        for surline_info in ast.literal_eval(surline_information):
+            words = surline_info[0]
+            index = surline_info[1]
+            for i in range(len(words)):
+                df_surline_batch = df_surline_batch.append({'word':words[i], 'coordinates':index[i]}, ignore_index=True)
+        df_surline_batch.sort_values(by="coordinates", inplace=True) # for surlining in order of position
+
+        # Updating the report
+
+        for i in range(df_surline_batch.shape[0]):
+            word = df_surline_batch['word'].iloc[i]
+            index = df_surline_batch['coordinates'].iloc[i]
+
+            report = report[:index + shift] + start_tag + word + end_tag + report[len(report[:index + shift] + word):]
+            
+            shift += len(start_tag) + len(end_tag)
+        return report
+
+    except:
+        return report
+
+def extract_info(filepath_filter, dataset):
+    filtered_dataset = dataset[dataset.index == filepath_filter]
+
+    report = filtered_dataset['report'].iloc[0]
+    hashtags = filtered_dataset['hashtag'].iloc[0]
+    crisis_type = filtered_dataset['crisis_type'].iloc[0]
+    surline_data = filtered_dataset['surline_data'].iloc[0]
+
+    return report, hashtags, crisis_type, surline_data
 
 
+# Listing all the index names
+filepaths = dataset.index
 
-filepaths = data.index
-
+# Creating the index selector
 st.subheader('Select index')
 filepath_filter = st.selectbox('filepath',filepaths)
 
-# Filters
-filtered_data = data[data.index == filepath_filter]
-filtererd_report = filtered_data['report'].iloc[0]
-filtererd_hashtags = filtered_data['hashtag'].iloc[0]
-filtererd_crisis_type = filtered_data['crisis_type'].iloc[0]
-rich_text = '<span style="background-color: #ff5733">LENGTH OF THE RECORDING</span>: 22  ***minutes*** and 53 <em>seconds</em> CLINICAL HISTORY: The patient is a 27-year-old man with past medical history of cognitive delay, mitochondrial disorder, seizure disorder. The patient was brought to the emergency room with multiple witnessed seizures. MEDICATIONS: Ativan, Dilantin, clonazepam, chlorpromazine, amlodipine. INTRODUCTION: Digital video EEG is performed in the lab/bedside using standard 10-20 system of electrode placement with one channel of EKG. Hyperventilation and photic stimulation are performed. DESCRIPTION OF THE RECORD: During maximal wakefulness there are periods of fragmented moderate amplitude 9 Hz alpha activity with preserved anterior-to-posterior frequency amplitude gradient. The majority of the background, however, consists of low amplitude fast activity admixed with polymorphic frequently sharply contoured central theta and delta activity. Additionally, up to 20 second runs of sharply contoured frontocentral theta activity without spatial or temporal evolution are captured. These runs are not ictal in etiology. Occasional moderate amplitude isolated diffuse frontally predominant sharply contoured slow waves without clear spike waves or sharp waves are captured. A good example of such waveform occurs at 09:59:50 a.m. On a single occasion at 10:01:20 a.m., such discharge is correlated with a brief generalized body jerk. There is a continuous diffuse EKG artifact. No hyperventilation or photic stimulation are captured. There is no normal sleep architecture captured. FINDINGS: 1. Mild diffuse slowing. 2. Runs of non-evolving frontocentral theta activity. 3. Isolated diffuse frontally predominant slow waves that on a single occasion correlates with the generalized body jerks. IMPRESSION: This is a very abnormal EEG due to mild diffuse slowing, runs of sharply contoured bifrontocentral activity and occasional diffuse waveforms without clearly formed sharp waveforms that on a single occasion are associated with brief generalized body jerks. This EEG supports a bihemispheric diffuse disturbance of cerebral dysfunction. No clear electrographic seizures are captured.'
-#rich_text = '<span style="background-color: #FFFF00">This text is highlighted in yellow.</span>'
+report, hashtags, crisis_type, surline_data = extract_info(filepath_filter, dataset)
+
+md_text = create_markdown_text(report, surline_data)
+
+# Display of the elements
+
 st.subheader('crisis type')
-st.write(filtererd_crisis_type)
+st.write(crisis_type)
 
 st.subheader('hashtags')
-st.write(filtererd_hashtags)
+st.write(hashtags)
 
 st.subheader('Report')
-st.write(filtererd_report)
-
-st.subheader('test rich text')
-st.markdown(rich_text, unsafe_allow_html=True)
+st.markdown(md_text, unsafe_allow_html=True)
