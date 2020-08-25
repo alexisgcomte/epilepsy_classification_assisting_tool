2769a398138608279ed2c40f3aebd5f351954b8c
alexisgcomte <alexis.g.comte@gmail.com>
2020-08-25 21:02:16 +0200

    misc cleaning

--- app.py
+++ app.py
@@ -4,45 +4,7 @@ import numpy as np
 import re
 import ast
 
-@st.cache
-def load_data():
-    data = pd.read_csv('data/structured_reports/Sample_annotated_report_database.csv', encoding="iso-8859-1")
-    return data
-
-#@st.cache(allow_output_mutation=True)
-def load_classified_reports():
-    data = pd.read_csv('data/classified_reports/classified_report_database.csv', encoding="iso-8859-1")
-    return data
-
-@st.cache
-def load_tags_list():
-    data = pd.read_csv('data/parameters/tags_list.csv', encoding="iso-8859-1")
-    tags_list = data['tags'].tolist()
-    return tags_list
-
-@st.cache
-def load_epilepsy_types_list():
-    data = pd.read_csv('data/parameters/epilepsy_types.csv', encoding="iso-8859-1")
-    epilepsy_type_list = data['epilepsy_types'].tolist()
-    return epilepsy_type_list
-
-#@st.cache(allow_output_mutation=True)
-def last_patient_classified():
-    last_patient_classified_df = pd.read_csv('data/parameters/last_patient_classified.csv', encoding="iso-8859-1")
-    last_patient_classified = last_patient_classified_df['last_patient_classified'].iloc[0]
-    return last_patient_classified_df, last_patient_classified
-
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.sidebar.info('Loading data...')
-dataset = load_data()
-classified_dataset = load_classified_reports()
-tags_list = load_tags_list()
-epilepsy_type_list = load_epilepsy_types_list()
-
-# Notify the reader that the data was successfully loaded.
-data_load_state.success("Data Loaded in cache successfully!")
-
-st.title('Patient epilepsy classification')
+# FUNCTION DEFINITIONS
 
 def create_highlighted_markdown_text(report, highlighted_information):
     #try :
@@ -113,8 +75,8 @@ def extract_info(selected_patient, dataset):
     single_patient_df = single_patient_df.sort_values(["Nb_Seizures", "Exam_name"], ascending = (False, True))
     return single_patient_df
 
-def default_value_listing(defaut_values_df):
-    
+def defaut_value_listing(defaut_values_df):
+    # Loading previously input datas
     # Dealing with Seizure_type and Tags colums
     default_list = []
     columns = ['Seizure_type', 'Tags']
@@ -139,22 +101,25 @@ def default_value_listing(defaut_values_df):
     return [element for element in default_list]      
 
 def extract_defaut_values(selected_patient, classified_dataset):
+    # Extract previously input fields
     defaut_values_df = classified_dataset[classified_dataset["Patient_name"] == selected_patient]
-    default_epilepsy_type, default_tags, default_free_notes = default_value_listing(defaut_values_df)
+    default_epilepsy_type, default_tags, default_free_notes = defaut_value_listing(defaut_values_df)
     return default_epilepsy_type, default_tags, default_free_notes
 
 def update_classified_dataset(selected_patient, classified_dataset, epilepsy_type_input, keywords_input, free_notes_input):
+    # Update the classification CSV with input values
     classified_dataset.loc[classified_dataset['Patient_name'] == selected_patient, 'Seizure_type'] = re.sub(r"([\]\'\[])",'',str(epilepsy_type_input))
     classified_dataset.loc[classified_dataset['Patient_name'] == selected_patient, 'Tags'] = re.sub(r"([\]\'\[])",'',str(keywords_input))
     classified_dataset.loc[classified_dataset['Patient_name'] == selected_patient, 'Free_Notes'] = free_notes_input
-
     return classified_dataset
 
 def update_last_patient_classified(last_patient_classified_df, selected_patient):
+    # Update the number of the last patient classified
     last_patient_classified_df['last_patient_classified'].iloc[0] = sorted_list[sorted_list.index(selected_patient)]
     last_patient_classified_df.to_csv('data/parameters/last_patient_classified.csv', index=False)
 
 def update_last_patient_classified_next(last_patient_classified_df, selected_patient):
+    # Modify last_patient_classied for the next before loading
     if selected_patient == sorted_list[-1]:
         updated_patient = sorted_list[sorted_list.index(selected_patient)]
         st.sidebar.info('Last report completed!')
@@ -165,6 +130,7 @@ def update_last_patient_classified_next(last_patient_classified_df, selected_pat
     return updated_patient
 
 def update_last_patient_classified_previous(last_patient_classified_df, selected_patient):
+    # Modify last_patient_classied for the previous before loading
     if selected_patient == sorted_list[0]:
         updated_patient = sorted_list[sorted_list.index(selected_patient)]
         st.sidebar.info('First report :)')
@@ -174,7 +140,8 @@ def update_last_patient_classified_previous(last_patient_classified_df, selected
     last_patient_classified_df.to_csv('data/parameters/last_patient_classified.csv', index=False)
     return updated_patient
 
-def completion_status(epilepsy_type_input, keywords_input, free_notes_input):
+def completion_status(epilepsy_type_input):
+    # Return 1 of Epilepsy type is incomplete, else 0
     status = 1
     epilepsy_type_input = re.sub(r"([\]\'\[])",'',str(epilepsy_type_input))
 
@@ -182,7 +149,47 @@ def completion_status(epilepsy_type_input, keywords_input, free_notes_input):
         status = 0
     return status
 
-# Creating the patient index selector in side menu
+# LOADING THE DATAS
+
+@st.cache
+def load_data():
+    data = pd.read_csv('data/structured_reports/Sample_annotated_report_database.csv', encoding="iso-8859-1")
+    return data
+
+#@st.cache(allow_output_mutation=True)
+def load_classified_reports():
+    data = pd.read_csv('data/classified_reports/classified_report_database.csv', encoding="iso-8859-1")
+    return data
+
+@st.cache
+def load_tags_list():
+    data = pd.read_csv('data/parameters/tags_list.csv', encoding="iso-8859-1")
+    tags_list = data['tags'].tolist()
+    return tags_list
+
+@st.cache
+def load_epilepsy_types_list():
+    data = pd.read_csv('data/parameters/epilepsy_types.csv', encoding="iso-8859-1")
+    epilepsy_type_list = data['epilepsy_types'].tolist()
+    return epilepsy_type_list
+
+#@st.cache(allow_output_mutation=True)
+def last_patient_classified():
+    last_patient_classified_df = pd.read_csv('data/parameters/last_patient_classified.csv', encoding="iso-8859-1")
+    last_patient_classified = last_patient_classified_df['last_patient_classified'].iloc[0]
+    return last_patient_classified_df, last_patient_classified
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.sidebar.info('Loading data...')
+dataset = load_data()
+classified_dataset = load_classified_reports()
+tags_list = load_tags_list()
+epilepsy_type_list = load_epilepsy_types_list()
+
+# Notify the reader that the data was successfully loaded.
+data_load_state.success("Data Loaded in cache successfully!")
+
+# SIDEBAR WINDOWS
 
 unique_patient_ids = set(dataset["Patient_name"])
 sorted_list = sorted(list(unique_patient_ids))
@@ -190,7 +197,10 @@ sorted_list = sorted(list(unique_patient_ids))
 last_patient_classified_df, last_patient_classified = last_patient_classified()
 selected_patient = last_patient_classified
 
+# Patient ID navigation
+
 st.sidebar.subheader('Patient ID navigation:')
+
 if st.sidebar.button('Next'):
     selected_patient = update_last_patient_classified_next(last_patient_classified_df, selected_patient)
 
@@ -198,10 +208,7 @@ if st.sidebar.button('Previous'):
     selected_patient = update_last_patient_classified_previous(last_patient_classified_df, selected_patient)
 
 selected_patient = st.sidebar.selectbox('Manual Selection  ', sorted_list, index=sorted_list.index(selected_patient))
-
 single_patient_df = extract_info(selected_patient, dataset)
-
-st.subheader('Current patient ID is: {}'.format(selected_patient))
 
 # Manual classification part
 
@@ -212,16 +219,22 @@ epilepsy_type_input = st.sidebar.multiselect('Epilepsy type input', epilepsy_typ
 keywords_input = st.sidebar.multiselect('Keywords input', tags_list, default=default_tags)
 free_notes_input = st.sidebar.text_area('Free notes', value=default_free_notes)
 
-status = completion_status(default_epilepsy_type, default_tags, default_free_notes)
+status = completion_status(default_epilepsy_type)
 
 if st.sidebar.button('Save'):
     classified_dataset = update_classified_dataset(selected_patient, classified_dataset, epilepsy_type_input, keywords_input, free_notes_input)
     classified_dataset.to_csv('data/classified_reports/classified_report_database.csv', index=False)
     update_last_patient_classified(last_patient_classified_df, selected_patient)
-    status = completion_status(epilepsy_type_input, keywords_input, free_notes_input)
+    # Checking if report is now completed
+    status = completion_status(epilepsy_type_input)
+
     data_save_state = st.sidebar.info('Saving data...')
     data_save_state.success("Classification saved!")
 
+# MAIN WINDOW
+
+st.title('Patient epilepsy classification')
+st.subheader('Current patient ID is: {}'.format(selected_patient))
 
 if status == 1:
     st.subheader('Status: {}'.format('Classification completed'))
@@ -246,7 +259,4 @@ for index, row in single_patient_df.iterrows():
 
     #Display highlighted repport
     md_report = create_highlighted_markdown_text(row["Patient_report"], row["Highlighted_data"])
-    st.markdown(md_report, unsafe_allow_html=True) 
-
-
-
+    st.markdown(md_report, unsafe_allow_html=True)
