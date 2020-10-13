39c2e808a7c03d9fb5dc6a4a543344016d99f1d5
alexisgcomte <alexis.g.comte@gmail.com>
2020-10-13 16:41:27 +0200

    adding installation process and requirements.txt

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # manual_classification_help_app
 
+## Installation
+
+Creating the virtual environnement with all the dependencies:
+```bash
+python3 -m venv env_streamlit
+source env_streamlit/bin/activate
+pip3 install -r requirements.txt
+```
+
+## Starting the app
+
+```bash
+source env_streamlit/bin/activate
+streamlit run app.py
+```
+
 ## Status
 
 App is availiable for testing!
@@ -10,3 +26,4 @@ Data samples are extracted from Temple University EEG Seizure corpus.
 **References**
 
 Obeid I., Picone J. (2016). The temple university hospital EEG data corpus. Front. Neurosci. 10:196. 10.3389/fnins.2016.00196 [[PMC free article](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4865520/)] [[PubMed](https://pubmed.ncbi.nlm.nih.gov/27242402/)] [[CrossRef](https://www.frontiersin.org/articles/10.3389/fnins.2016.00196/full)] [[Google Scholar](https://scholar.google.com/scholar_lookup?journal=Front.+Neurosci.&title=The+temple+university+hospital+EEG+data+corpus&author=I.+Obeid&author=J.+Picone&volume=10&publication_year=2016&pages=196&pmid=27242402&doi=10.3389/fnins.2016.00196&)]
+
--- app.py
+++ app.py
@@ -151,6 +151,7 @@ def update_classified_dataset(selected_patient, classified_dataset, epilepsy_typ
     classified_dataset.loc[classified_dataset['Patient_name'] == selected_patient, 'Laterality'] = re.sub(r"([\]\'\[])",'',str(laterality_input))
     classified_dataset.loc[classified_dataset['Patient_name'] == selected_patient, 'Free_Notes'] = free_notes_input
     classified_dataset.loc[classified_dataset['Patient_name'] == selected_patient, 'thesaurus'] = re.sub(r"([\]\\[])",'',str(thesaurus_input))
+    classified_dataset.loc[classified_dataset['Patient_name'] == selected_patient, 'classified'] = 1
     return classified_dataset
 
 def update_last_patient_classified(last_patient_classified_df, selected_patient):
@@ -182,14 +183,24 @@ def update_last_patient_classified_previous(last_patient_classified_df, selected
 ###
 # TO UPDATE
 ###
-def completion_status(thesaurus_input):
-    # Return 1 of Epilepsy type is incomplete, else 0
-    status = 1
-    thesaurus_input = re.sub(r"([\]\'\[])",'',str(thesaurus_input))
+#def completion_status(thesaurus_input):
+#    # Return 1 of Epilepsy type is incomplete, else 0
+#    status = 1
+#    thesaurus_input = re.sub(r"([\]\'\[])",'',str(thesaurus_input))
+#
+#    if (len(thesaurus_input) == 0 or str(thesaurus_input) == 'None'):
+#        status = 0
+#    return status
 
-    if (len(thesaurus_input) == 0 or str(thesaurus_input) == 'None'):
-        status = 0
-    return status
+
+def completion_status(classified_dataset, selected_patient):
+    if classified_dataset[classified_dataset['Patient_name'] == selected_patient]['classified'].iloc[0] == 1:
+        return 1
+    else:
+        return 0
+
+
+
 
 # LOADING THE DATAS
 
@@ -201,7 +212,7 @@ def update_save_path():
 
 @st.cache
 def load_data():
-    data = pd.read_csv('data/structured_reports/Sample_annotated_report_database.csv', encoding="iso-8859-1")
+    data = pd.read_csv('data/structured_reports/Sample_annotated_report_database.csv', encoding='utf-8')
     #data = pd.read_csv('data/structured_reports/Annotated_reports_database_tagged_v0.2 tag utf-8.csv', encoding='utf8', sep=";")
     return data
 
@@ -212,37 +223,37 @@ def load_classified_reports_first(save_path):
     return data
 
 #@st.cache(allow_output_mutation=True)
-def load_classified_reports(save_path):
+def load_classified_reports():
     data = pd.read_csv('data/classified_reports/classified_report_database.csv', encoding='UTF-8')
     return data
 
 @st.cache
 def load_tags_list():
-    data = pd.read_csv('data/parameters/tags_list.csv', encoding="iso-8859-1")
+    data = pd.read_csv('data/parameters/tags_list.csv', encoding="utf-8")
     tags_list = data['tags'].tolist()
     return tags_list
 
 @st.cache
 def load_epilepsy_types_list():
-    data = pd.read_csv('data/parameters/epilepsy_types.csv', encoding="iso-8859-1")
+    data = pd.read_csv('data/parameters/epilepsy_types.csv', encoding="utf-8")
     epilepsy_type_list = data['epilepsy_types'].tolist()
     return epilepsy_type_list
 
 @st.cache
 def load_laterality_list():
-    data = pd.read_csv('data/parameters/laterality_list.csv', encoding="iso-8859-1")
+    data = pd.read_csv('data/parameters/laterality_list.csv', encoding="utf-8")
     laterality_list = data['laterality'].tolist()
     return laterality_list
 
 @st.cache
 def load_neutral_tags_list():
-    data = pd.read_csv('data/parameters/neutral_tags_list.csv', encoding="iso-8859-1")
+    data = pd.read_csv('data/parameters/neutral_tags_list.csv', encoding="utf-8")
     neutral_tags_list = data['neutral_tags'].tolist()
     return neutral_tags_list
 
 @st.cache
 def load_thesaurus_list():
-    data = pd.read_csv('data/parameters/thesaurus_list.csv', encoding="iso-8859-1")
+    data = pd.read_csv('data/parameters/thesaurus_list.csv', encoding="utf-8")
     thesaurus_list = data['thesaurus'].tolist()
     return thesaurus_list
 
@@ -254,7 +265,7 @@ def simplified_key_words():
 
 #@st.cache(allow_output_mutation=True)
 def last_patient_classified():
-    last_patient_classified_df = pd.read_csv('data/parameters/last_patient_classified.csv', encoding="iso-8859-1")
+    last_patient_classified_df = pd.read_csv('data/parameters/last_patient_classified.csv', encoding="utf-8")
     last_patient_classified = last_patient_classified_df['last_patient_classified'].iloc[0]
     return last_patient_classified_df, last_patient_classified
 
@@ -271,7 +282,7 @@ data_load_state = st.sidebar.info('Loading data...')
 save_path = update_save_path()
 dataset = load_data()
 classified_dataset = load_classified_reports_first(save_path)
-classified_dataset = load_classified_reports(save_path)
+classified_dataset = load_classified_reports()
 tags_list = load_tags_list()
 epilepsy_type_list = load_epilepsy_types_list()
 laterality_list = load_laterality_list()
@@ -343,17 +354,25 @@ def index_thesaurus_list(default_thesaurus):
 
 thesaurus_input = st.sidebar.selectbox('Epilepsy Classification ', thesaurus_list, index=thesaurus_list.index(index_thesaurus_list(default_thesaurus)))
 
-status = completion_status(default_thesaurus)
+status = completion_status(classified_dataset, selected_patient)
 
 if st.sidebar.button('Save'):
     classified_dataset = update_classified_dataset(selected_patient, classified_dataset, epilepsy_type_input, keywords_input, laterality_input, thesaurus_input, free_notes_input)
     classified_dataset.to_csv('data/classified_reports/classified_report_database.csv', index=False, encoding='UTF-8')
     update_last_patient_classified(last_patient_classified_df, selected_patient)
     # Checking if report is now completed
-    status = completion_status(thesaurus_input)
+    status = completion_status(classified_dataset, selected_patient)
 
     data_save_state = st.sidebar.info('Saving data...')
     data_save_state.success("Classification saved!")
+
+if st.sidebar.button('Reviewed'):
+    classified_dataset.loc[classified_dataset['Patient_name'] == selected_patient, 'classified'] = 1
+    classified_dataset.to_csv('data/classified_reports/classified_report_database.csv', index=False, encoding='UTF-8')
+    update_last_patient_classified(last_patient_classified_df, selected_patient)
+    status = completion_status(classified_dataset, selected_patient)
+    data_save_state = st.sidebar.info('Saving data...')
+    data_save_state.success("Classification reviewed!")
 
 # MAIN WINDOW
 st.title('Patient epilepsy classification')
